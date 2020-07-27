eaf87287d1264ee4ae868ab8bbd1090544d5d130
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-07-28 00:44:21 +0530

    conditional template rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,6 +6,12 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   }
 
+  renderTags() {
+    return this.state.tags.length === 0 ?
+      <p>There is no tags</p> :
+      this.state.tags.map(tag => <li key={tag}>{tag}</li>);
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -14,7 +20,7 @@ class Counter extends Component {
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
         <ul>
-          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+          {this.renderTags()}
         </ul>
       </React.Fragment>
     );
